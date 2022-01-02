19102ab6f860cba6901dbe7d0fdf5fd067d8841f
Fabio Kindinger <96880885+Fabio-Kindinger@users.noreply.github.com>
2022-01-02 17:39:50 -0300

    Initial commit

---
+++ Aula10/ex008.js
@@ -0,0 +1,12 @@
+var vel = 72.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+       if (vel > 60){
+        console.log(`Você ultrapssou a velocidade permitida. MULTADO !!!`)
+       } 
+console.log(`Use sempre cinto de segurança !`)
+
+
+
+
+
+
